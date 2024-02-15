14ba89199d56799d453e9984180b9b87a294934d
n0ted <bhadauriagaurav22@gmail.com>
2024-02-15 21:58:07 +0530

    test

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     siteName: "NA",
     images: [
       {
-        url: "https://chronark.com/og.png",
+        url: "chronark.com/public/og.png",
         width: 1920,
         height: 1080,
       },
